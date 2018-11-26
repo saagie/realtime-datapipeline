a5828ff5afcf33aa3b164aec1e8670cc08ec7c72
Erwan KOFFI <erwan@saagie.com>
2018-11-26 11:22:41 +0100

    - solutions

--- src/main/scala/io/saagie/academy/datapipeline/spark/streaming/Streaming.scala
+++ src/main/scala/io/saagie/academy/datapipeline/spark/streaming/Streaming.scala
@@ -6,7 +6,7 @@ import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming.dstream.{DStream, InputDStream}
 import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
-import org.apache.spark.streaming.kafka010.KafkaUtils
+import org.apache.spark.streaming.kafka010.{ConsumerStrategy, KafkaUtils}
 import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
 import org.apache.spark.streaming.{Seconds, StreamingContext, kafka010}
 import org.json4s.DefaultFormats
@@ -26,38 +26,23 @@ case object Streaming {
   def topic = "tweet"
 
   def createConfiguration: Option[SparkConf] = {
-    val conf = Some(new SparkConf()
-      .setAppName(appName)
-      .setMaster(master)
-      .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
-      .set("spark.driver.allowMultipleContexts", "true"))
+    val conf = None
     conf
   }
 
   def createKafkaConfiguration: Option[Map[String, Object]] = {
-    val conf = Some(Map[String, Object](
-      "bootstrap.servers" -> broker,
-      "key.deserializer" -> classOf[StringDeserializer],
-      "value.deserializer" -> classOf[StringDeserializer],
-      "group.id" -> groupId,
-      "auto.offset.reset" -> "earliest",
-      "enable.auto.commit" -> (false: java.lang.Boolean)
-    ))
+    val conf = None
     conf
   }
 
   def createStreamingContext: Option[StreamingContext] = {
-    val ssc = new StreamingContext(createConfiguration.get, interval)
-    Some(ssc)
+    val ssc = None
+    ssc
   }
 
   def createKafkaDirectStream: Option[InputDStream[ConsumerRecord[String, String]]] = {
-    val subscription = createKafkaConfiguration.fold(Option.empty[kafka010.ConsumerStrategy[String, String]])(conf => Some(Subscribe[String, String](Array(topic), conf)))
-    val stream = Some(KafkaUtils.createDirectStream[String, String](
-      createStreamingContext.get,
-      PreferConsistent,
-      subscription.get
-    ))
+    val subscription: Option[ConsumerStrategy[String, String]] = None
+    val stream = None
     stream
   }
 
@@ -67,10 +52,6 @@ case object Streaming {
   }
 
   def transformToMessage(stream: DStream[String]): DStream[Int] = {
-    stream
-      .map(record => {
-        stringToMessage(record)
-      })
-      .map(_.text.split(" ").length)
+    stream.map(_ => 1)
   }
 }
--- src/main/scala/io/saagie/academy/datapipeline/spark/structured/StructuredStreaming.scala
+++ src/main/scala/io/saagie/academy/datapipeline/spark/structured/StructuredStreaming.scala
@@ -12,29 +12,18 @@ case object StructuredStreaming {
   def broker = "localhost:9092"
 
   def createSession: Option[SparkSession] = {
-    val spark = Some(SparkSession.builder()
-      .appName(appName)
-      .master(master)
-      .getOrCreate())
+    val spark = None
     spark
   }
 
   def readDataSet(sparkSession: SparkSession): Option[Dataset[Message]] = {
     import sparkSession.implicits._
-    val ds = Some(sparkSession
-      .readStream
-      .format("kafka")
-      .option("bootstrap.servers", broker)
-      .option("subscribe", "topic")
-      .load()
-      .select(from_json($"value", Encoders.product[Message].schema) as "value")
-      .select($"value.*")
-      .as[Message])
+    val ds = None
     ds
   }
 
   def countWords(df: Dataset[Message]): Option[Dataset[Long]] = {
     import df.sparkSession.implicits._
-    Some(df.select(count(split($"text", " "))).as[Long])
+    None
   }
 }
